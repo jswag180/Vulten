76e217c9e957d39312039f8c9c693bae178b1c08
jswag180 <jefferycole100@gmail.com>
2023-07-10 11:11:53 -0500

    Switch MatMulOp to new input/output

--- gpu/gpu_MatMul_op.cpp
+++ gpu/gpu_MatMul_op.cpp
@@ -43,27 +43,30 @@ void MatMulOp_Compute(void* kernel, TF_OpKernelContext* ctx) {
 
   StatusSafePtr status(TF_NewStatus());
 
-  GET_INPUT_TENSOR("MatMul", a, 0, ctx, status)
+  tensor_utills::Input_tensor a =
+      tensor_utills::get_input_tensor("MatMulOp:a", 0, ctx, status.get());
   vulten_ops::Mat_size a_mat_size = vulten_ops::Mat_size();
-  a_mat_size.x = a_dims[0];
-  a_mat_size.y = a_dims[1];
+  a_mat_size.x = a.dims[0];
+  a_mat_size.y = a.dims[1];
 
-  GET_INPUT_TENSOR("MatMul", b, 1, ctx, status)
+  tensor_utills::Input_tensor b =
+      tensor_utills::get_input_tensor("MatMulOp:b", 1, ctx, status.get());
   vulten_ops::Mat_size b_mat_size = vulten_ops::Mat_size();
-  b_mat_size.x = b_dims[0];
-  b_mat_size.y = b_dims[1];
+  b_mat_size.x = b.dims[0];
+  b_mat_size.y = b.dims[1];
 
-  uint32_t ax = matMulOpInfo->transA_ ? a_dims[1] : a_dims[0];
-  uint32_t ay = matMulOpInfo->transA_ ? a_dims[0] : a_dims[1];
-  uint32_t bx = matMulOpInfo->transB_ ? b_dims[1] : b_dims[0];
-  uint32_t by = matMulOpInfo->transB_ ? b_dims[0] : b_dims[1];
+  uint32_t ax = matMulOpInfo->transA_ ? a.dims[1] : a.dims[0];
+  uint32_t ay = matMulOpInfo->transA_ ? a.dims[0] : a.dims[1];
+  uint32_t bx = matMulOpInfo->transB_ ? b.dims[1] : b.dims[0];
+  uint32_t by = matMulOpInfo->transB_ ? b.dims[0] : b.dims[1];
   if (ay != bx) {
     std::cerr << "Error: incompatable sized matrixes\n";
     return;
   }
 
-  absl::InlinedVector<int64_t, 2> out_dims = {ax, by};
-  MAKE_OUTPUT_TENSOR("MatMul", output, 0, out_dims, T, ctx, status)
+  absl::InlinedVector<int64_t, 4> out_dims = {ax, by};
+  tensor_utills::Output_tensor output =
+      tensor_utills::make_output_tensor("MatMulOp:output", 0, out_dims, ctx, status.get());
 
   SP_Stream stream = TF_GetStream(ctx, status.get());
   vulten_backend::Instance* inst = stream->instance;
@@ -78,9 +81,9 @@ void MatMulOp_Compute(void* kernel, TF_OpKernelContext* ctx) {
   matMul_op = (vulten_ops::MatMul_op*)inst->op_chache[op_cache_name];
   inst->main_queue_mutex.unlock();
 
-  matMul_op->run_op((vulten_ops::Data_type)T, a_tensor, matMulOpInfo->transA_,
-                    a_mat_size, b_tensor, matMulOpInfo->transB_, b_mat_size,
-                    output_tensor);
+  matMul_op->run_op((vulten_ops::Data_type)T, a.vulten_tensor,
+                    matMulOpInfo->transA_, a_mat_size, b.vulten_tensor,
+                    matMulOpInfo->transB_, b_mat_size, output.vulten_tensor);
 }
 
 template <TF_DataType T>
