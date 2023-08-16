8cc06a8507d184f47c32191ffc0fa3284a303347
jswag180 <jefferycole100@gmail.com>
2023-08-16 14:34:52 -0500

    BiasAddGrad: use less evil way of handling format

--- gpu/gpu_BiasAddGrad_op.cpp
+++ gpu/gpu_BiasAddGrad_op.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 
 #include "Vulten_backend/ops/reduce/Reduce_op.h"
@@ -11,8 +12,7 @@
 #include "vulten_device.h"
 
 struct BiasAddGradOp {
-  BiasAddGradOp() : format("AAAA") {}
-  std::string format;
+  char format[5];
 };
 
 void* BiasAddGradOp_Create(TF_OpKernelConstruction* ctx) {
@@ -20,8 +20,8 @@ void* BiasAddGradOp_Create(TF_OpKernelConstruction* ctx) {
 
   StatusSafePtr status(TF_NewStatus());
 
-  TF_OpKernelConstruction_GetAttrString(ctx, "data_format",
-                                        kernel->format.data(), 4, status.get());
+  TF_OpKernelConstruction_GetAttrString(ctx, "data_format", kernel->format, 5,
+                                        status.get());
 
   return kernel;
 }
@@ -46,13 +46,13 @@ void BiasAddGradOp_Compute(void* kernel, TF_OpKernelContext* ctx) {
   std::vector<int32_t> axis_vec = std::vector<int32_t>();
 
   absl::InlinedVector<int64_t, 4> out_dims(1);
-  if (biasAddGradOp_info->format == "NHWC") {
+  if (std::strcmp(biasAddGradOp_info->format, "NHWC") == 0) {
     out_dims[0] = input.dims[input.dims.size() - 1];
     for (uint32_t i = 0; i < input.dims.size() - 1; i++) {
       axis_vec.push_back(i);
     }
     std::reverse(axis_vec.begin(), axis_vec.end());
-  } else if (biasAddGradOp_info->format == "NCHW") {
+  } else if (std::strcmp(biasAddGradOp_info->format, "NCHW") == 0) {
     out_dims[0] = input.dims[1];
     for (uint32_t i = 0; i < input.dims.size(); i++) {
       if (i != 1) {
